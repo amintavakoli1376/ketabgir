e6cf69499c53c8bcbb04422fadc782e5832be40b
amintavakoli <amin76tavakoli76@gmail.com>
2022-03-27 15:58:47 +0430

    category and author fixed

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ArticleList, ArticleDetail, CategoryList, AuthorList, ArticlePreview, SearchList, LikeView, newsletter_signup, newsletter_unsubscribe
+from .views import ArticleList, ArticleDetail, ArticlePreview, SearchList, LikeView, newsletter_signup, newsletter_unsubscribe
 from . import views
 
 app_name = 'blog'
@@ -8,12 +8,14 @@ urlpatterns = [
     path('page/<int:page>', ArticleList.as_view(), name='home'),
     path('articles/<slug:slug>', ArticleDetail.as_view(), name='detail'),
     path('preview/<int:pk>', ArticlePreview.as_view(), name='preview'),
-    path('category/<slug:slug>', CategoryList.as_view(), name='category'),
-    path('category/<slug:slug>/page/<int:page>',
-         CategoryList.as_view(), name='category'),
-    path('author/<slug:username>', AuthorList.as_view(), name='author'),
-    path('author/<slug:username>/page/<int:page>',
-         AuthorList.as_view(), name='author'),
+    #     path('category/<slug:slug>', CategoryList.as_view(), name='category'),
+    #     path('category/<slug:slug>/page/<int:page>',
+    #          CategoryList.as_view(), name='category'),
+    path('category/<slug:slug>', views.category, name='category'),
+    #     path('author/<slug:username>', AuthorList.as_view(), name='author'),
+    #     path('author/<slug:username>/page/<int:page>',
+    #          AuthorList.as_view(), name='author'),
+    path('author/<slug:username>', views.author, name='author'),
     path('search/', SearchList.as_view(), name='search'),
     path('search/page/<int:page>', SearchList.as_view(), name='search'),
     path('like/<slug:slug>', LikeView, name='like_post'),
--- blog/views.py
+++ blog/views.py
@@ -56,36 +56,48 @@ class ArticlePreview(AuthorAccessMixin, DetailView):
         return get_object_or_404(Article, pk=pk)
 
 
-class CategoryList(ListView):
-    paginate_by = 9
-    template_name = "blog/category_list.html"
+# class CategoryList(ListView):
+#     paginate_by = 9
+#     template_name = "blog/category_list.html"
 
-    def get_queryset(self):
-        global category
-        slug = self.kwargs.get('slug')
-        category = get_object_or_404(Category.objects.active(), slug=slug)
-        return category.articles.published()
+#     def get_queryset(self):
+#         global category
+#         slug = self.kwargs.get('slug')
+#         category = get_object_or_404(Category.objects.active(), slug=slug)
+#         return category.articles.published()
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["category"] = category
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["category"] = category
+#         return context
+
+def category(request, slug=None):
+    context = {
+        "category": get_object_or_404(Category, slug=slug, status=True)
+    }
+    return render(request, "blog/category_list.html", context)
 
 
-class AuthorList(ListView):
-    paginate_by = 9
-    template_name = "blog/author_list.html"
+# class AuthorList(ListView):
+#     paginate_by = 9
+#     template_name = "blog/author_list.html"
 
-    def get_queryset(self):
-        global author
-        username = self.kwargs.get('username')
-        author = get_object_or_404(User, username=username)
-        return author.articles.published()
+#     def get_queryset(self):
+#         global author
+#         username = self.kwargs.get('username')
+#         author = get_object_or_404(User, username=username)
+#         return author.articles.published()
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["author"] = author
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["author"] = author
+#         return context
+
+def author(request, username=None):
+    context = {
+        "author": get_object_or_404(User, username=username)
+    }
+    return render(request, "blog/author_list.html", context)
 
 
 class SearchList(ListView):
